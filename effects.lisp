b119fdd6db17a43a5064d80731f3e081541b9a0a
root <root@joel-laptop.(none)>
2009-05-31 13:41:59 +0530

    Simple theming

---
+++ effects.lisp
@@ -0,0 +1,11 @@
+(in-package :stumpwm)
+
+(setf *message-window-gravity* :top-right)
+(setf *input-window-gravity* :top-right)
+
+(set-font "-b&h-lucidatypewriter-medium-r-normal-sans-11-80-100-100-m-70-iso10646-1")
+(set-bg-color "black")
+(set-fg-color "antiquewhite")
+(set-border-color "lightgreen")
+(set-unfocus-color "darkgray")
+(setf *window-border-style* :thin)
